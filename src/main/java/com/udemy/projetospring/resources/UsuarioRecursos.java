d016673b5d99ca661cb1e4d6888c0ba09e4e22f9
Murillo <murillom.martinho@gmail.com>
2021-01-11 17:56:58 -0300

    Adicionada camada de Serviços Usuario

--- src/main/java/com/udemy/projetospring/resources/UsuarioRecursos.java
+++ src/main/java/com/udemy/projetospring/resources/UsuarioRecursos.java
@@ -1,9 +1,14 @@
 package com.udemy.projetospring.resources;
 
-import com.udemy.projetospring.entities.Usuario;
+import java.util.List;
 
+import com.udemy.projetospring.entities.Usuario;
+import com.udemy.projetospring.services.UsuarioServico;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UsuarioRecursos {
     
+    @Autowired
+    private UsuarioServico servico;
+
     @GetMapping
-    public ResponseEntity<Usuario> listarUsuarios(){
-        Usuario usuario = new Usuario(1L, "João", "joao@gmail.com", "999999", "123456");
-        return ResponseEntity.ok().body(usuario);
+    public ResponseEntity<List<Usuario>> listarUsuarios(){
+        List<Usuario> usuarios = servico.findAll();
+        return ResponseEntity.ok().body(usuarios);
     }
 
+    @GetMapping(value = "/{id}")
+    public Usuario listarPorId(@PathVariable Long id){
+        return servico.findById(id);
+    }
 }
---
+++ src/main/java/com/udemy/projetospring/services/UsuarioServico.java
@@ -0,0 +1,26 @@
+package com.udemy.projetospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.udemy.projetospring.entities.Usuario;
+import com.udemy.projetospring.repositories.UsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioServico {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(Long id){
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        return usuario.get();
+    }
+}
